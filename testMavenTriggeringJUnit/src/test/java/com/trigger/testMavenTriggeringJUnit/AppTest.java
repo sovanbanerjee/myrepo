bb11adad1d871f32a729d0f88469f5d0c27b75f6
sovanbanerjee <here.sovan@gmail.com>
2021-08-05 15:29:05 +0530

    initial commit

---
+++ testMavenTriggeringJUnit/src/test/java/com/trigger/testMavenTriggeringJUnit/AppTest.java
@@ -0,0 +1,15 @@
+package com.trigger.testMavenTriggeringJUnit;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void testApp()
+    {
+        System.out.println("Ran successfully");
+    }
+}
